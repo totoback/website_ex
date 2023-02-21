e4a1d44dc330254893f3c3e678449e2f254a1794
지영 <toto.joung@gmail.com>
2023-02-21 14:17:37 +0900

    추가

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const subBtn = document.querySelector("#snb .btn_menu");
+const subMenu =document.querySelector("#snb .sub_menu");
+
+subBtn.addEventListener("click",function(){
+  if(subMenu.classList.contains("hide")){
+    subMenu.classList.remove("hide");
+  } else{
+    subMenu.classList.add("hide");
+  }
+})
